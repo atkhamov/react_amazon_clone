156c465eb0b490a89783517880875a4ece548dd0
atkhamov <atkhamov@gmail.com>
2020-07-16 22:31:16 +0600

    Banner is finished

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
+import "./Home.css";
+
+// rfce - this is the shortcut
 
 function Home() {
    return (
       <div className="home">
          <img className="home__image"
-            src="https://images-eu.ssl-images-amazon.com/images/G/020/digital" alt=""/>
+            src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="" />
       </div>
    )
 }
 
 export default Home
+
