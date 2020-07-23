fbb0fd4d9505c25d26f6ac1c29361b562a434d11
atkhamov <atkhamov@gmail.com>
2020-07-24 01:51:44 +0600

    reducer has been updated

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,6 @@
 export const initialState = {
    basket: [],
+   user: null,
 };
 
 const reducer = (state, action) => {
@@ -7,7 +8,10 @@ const reducer = (state, action) => {
    switch (action.type) {
       case 'ADD_TO_BASKET':
          // Logic for adding item to basket
-         return { state };
+         return { 
+            ...state,
+            basket: [...state.basket, action.item]
+          };
          break;
       case 'REMOVE_FROM_BASKET':
          // Logic for Removing item from basket
